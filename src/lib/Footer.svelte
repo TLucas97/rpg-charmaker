2235c8227304a2b134626297333100782caeae50
tarcisio <tarcisio.almeida197@gmail.com>
2023-03-24 20:03:39 -0300

    fixed footer adjustment

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,4 @@
-<main class="w-full bg-blue-200 text-gray-900 p-2">
+<main class="fixed bottom-0 w-full bg-blue-200 text-gray-900 p-2">
   <span class="flex items-center justify-center font-bold">
     All rights reserved &copy; 2023
   </span>
