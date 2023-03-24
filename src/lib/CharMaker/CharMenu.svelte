ab420d72dbc9f6e1de550990cba9787fa06d0557
tarcisio <tarcisio.almeida197@gmail.com>
2023-03-24 20:01:17 -0300

    footer fix

--- src/lib/CharMaker/CharMenu.svelte
+++ src/lib/CharMaker/CharMenu.svelte
@@ -5,7 +5,7 @@
   let currentState: string = "mainMenu";
 </script>
 
-<main class="w-full h-[85vh] flex items-center justify-center animate-fade">
+<main class="w-full h-[90vh] flex items-center justify-center animate-fade">
   {#if currentState === "mainMenu"}
     <div class="flex space-x-5 animate-fade">
       <button
